2d0887f9a4a09e93eb489d2ea5d81202d881ae72
ashB <ashb100@outlook.com>
2014-01-16 00:19:53 +0000

    Used the Revealing Module Pattern for the main function. The app renders content based on selection and gives feedback if the rows are correct, it doesn’t display score at the moment.

--- scripts/number-bonds.js
+++ scripts/number-bonds.js
@@ -1,97 +1,108 @@
 /**
  * Created by Ashnita on 30/12/2013.
- * @todo Try and use method chaining
- * @todo documentation
- * @todo testing framework - Karma
- * @todo do the scores - create another object
- * @todo flash message manager pattern - to display message(correct/incorrect/duplicate)
- *       and style it with css so that it fades away with some set delay
- * @todo implement score keeping
+ * Trying to use the Revealing Module Pattern
+ * @todo How do I access the public properties of numberBonds from Row??
  */
 window.onload = function() {
-    App.main();
+    numberBonds.init();
 };
-App = {
-    main: function() {
-        // use event bubbling, add an event listener on the document
-        document.addEventListener('click', this.handleEvent);
-    },
+var numberBonds = function () {
+    // configurable variables
+    var conf = {
+        maxNumForDouble: 20,
+        parentContainer: "content"
+    };
 
-    handleEvent: function() {
-        if (event.target.className == "storyLink") {
-            App.renderTheStory()
+    var parentEl;       // the parent DOM element where the content will be displayed
+    var storySelection; // will be set to the "data-num" attribute when user clicks a story selection (nav) link
+    var isDouble;       // will be set if story selection is Double
+
+
+    var init = function () {
+        setParentElement();
+        addEvents();
+        document.onclick = function () {
+            if (event.target.className === "storyLink") {
+                storySelection = event.target.getAttribute("data-num");
+                console.log(storySelection, "Story Selcected");
+                isDouble = (storySelection == conf.maxNumForDouble);
+                console.log(isDouble, "Is selection Double?");
+                renderStory();
+            }
+        };
+
+        console.dir(parentEl);
+    };
+
+    var setParentElement = function () {
+        parentEl = document.getElementById(conf.parentContainer);
+    };
+
+    var addEvents = function() {
+        // don't know why addEventListener doesn't add the event handler for 'onclick' event of parentEl
+        // try putting it in anonymous function
+        parentEl.addEventListener('click', function()
+        {
+            if (event.target.className === "storyLink") {
+            storySelection = event.target.getAttribute("data-num");
+            isDouble = (storySelection == conf.maxNumForDouble);
+            renderStory();
+        }});
+    };
+
+    var setStorySelection = function() {
+        console.dir(event);
+        if (event.target.className === "storyLink") {
+            storySelection = event.target.getAttribute("data-num");
+            isDouble = (storySelection == conf.maxNumForDouble);
+            renderStory();
         }
-    },
+    };
 
-    /**
-     * Renders the rows of equations on the element with id = "content".
-     * Clears the innerHTML of parent element first
-     *
-     */
-    renderTheStory: function() {
-        var parentEl = document.getElementById("content"),
-            storyOfNumber = event.target.getAttribute("data-num"),
-            isDouble = (storyOfNumber === '20'),
-            equation = null;
-
+    var renderStory = function() {
+        var row;
         // clear the content first
         parentEl.innerHTML = "";
         // print the title of story
-        renderElement(parentEl, 'h2', {innerHTML: 'Story Of : ' + (isDouble ? 'Double' : storyOfNumber), className: 'storyTitle'});
-        equation = new RowContainer(parentEl, storyOfNumber, isDouble);
-        equation.init();
+        //renderElement(parentEl, 'h2', {innerHTML: 'Story Of : ' + (isDouble ? 'Double' : storySelection), className: 'storyTitle'});
+        printTitle();
+        // print the lines of the story
+        for (var rowNum = 0; rowNum <= storySelection; rowNum++) {
+            row = new Row(rowNum, parentEl, storySelection);
+            row.createEquation();
+        }
+    };
+
+    var printTitle = function() {
+        console.log(storySelection, "Story Selected (in printTitle)");
+        renderElement(parentEl, 'p', {innerHTML: 'Story Of : ' + (isDouble ? 'Double' : storySelection), className: 'storyTitle'});
+    };
+
+    return {
+        init: init,
+        parentEl: parentEl,
+        storySelection: storySelection
     }
-};
+}();
 
-// takes in id of parent container, checks if its double or number bonds and renders the rows correctly for the
-// number of rows needed (the links for each number selection has a data attribute which stores the number
-// displays the score
-function RowContainer(parentContainer, num, isDouble) {
-    this.parentEl = parentContainer;
-    this.isDouble = isDouble;
-    this.storyOfNumber = num;
-    this.score = 0;
-}
 
-RowContainer.prototype.init = function() {
-    // create a new row for the number of rows needed
-    var row = null,
-        allTheRows = [];
-    for (var i=0; i <= this.storyOfNumber; i++) {
-        row = new Row(i, this.parentEl, this.isDouble, this.storyOfNumber);
-        // keep the row in an array
-        allTheRows.push(row);
-        console.dir(row);
-        row.init();
-    }
-};
-
-/**
- *
- * @param i {Number}: something
- * @param parentEl {HTMLElement}: element where the row will be rendered
- * @param isDouble {boolean}
- * @param storyOfNumber {string}
- * @constructor
- */
-function Row(i, parentEl, isDouble, storyOfNumber) {
+function Row(i, parentEl, storyOfNumber) {
     this.parentEl = parentEl;
     this.el = null; // is <div> element the row is in
     this.rowNumber = i;
-    this.isDouble = isDouble;
+    this.isDouble = (storyOfNumber === '20');
     this.storyOfNumber = storyOfNumber;
     this.operand1 = null;
     this.operand2 = null;
     this.answer = null;
 }
 
-Row.prototype.init = function() {
+Row.prototype.createEquation = function() {
     this.renderContainer();
     this.renderRow();
     this.addEvents();
 };
 
-// add row's <div> element to the parent container
 Row.prototype.renderContainer = function() {
     this.el = renderElement(this.parentEl, "div", {className: 'row'});
 };
@@ -104,16 +115,6 @@ function foreach(object, action) {
     }
 }
 
-/**
- *
- *
- * @param parentEl
- * @param elementName
- * @param attributeList
- * @returns {HTMLElement}
- *
- * @todo create foreach function to iterate over properties in object
- */
 function renderElement(parentEl, elementType, attributeList) {
     var el = document.createElement(elementType);
     if ( attributeList != undefined) {
@@ -126,10 +127,6 @@ function renderElement(parentEl, elementType, attributeList) {
     return el;
 }
 
-/**
- *
- *
- */
 Row.prototype.renderRow = function() {
     if (this.isDouble == true) {
         // operand1
@@ -160,10 +157,6 @@ Row.prototype.renderRow = function() {
     }
 };
 
-
-/**
- *
- */
 Row.prototype.checkAnswerCorrect = function() {
     if (this.operand1.value !== '' && this.operand2.value !== '') {
         if (parseInt(this.operand1.value) + parseInt(this.operand2.value) === parseInt(this.answer.value)) {
@@ -174,9 +167,6 @@ Row.prototype.checkAnswerCorrect = function() {
     }
 };
 
-/**
- *
- */
 Row.prototype.addEvents = function() {
     var self = this;
     this.el.addEventListener("change", function() {
